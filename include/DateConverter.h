72336bcbd909d2e96f9f526323ed417e253c7091
brusju <jakubkasprzak6899@gmail.com>
2021-01-16 17:07:20 +0100

    add DateConverter

---
+++ include/DateConverter.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+class DateConverter
+{
+public:
+    static std::string glue(std::string d, std::string m, std::string r)
+    {
+        std::stringstream stream;
+        stream << std::setfill ('0') 
+        << std::setw(2) << d
+        << std::setw(1) << "-" 
+        << std::setw(2) << m 
+        << std::setw(1) << "-"
+        << std::setw(4) << r;
+        return stream.str();
+    }
+};
