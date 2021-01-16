46daaf326027501cf986636780979ef7cefc63fe
brusju <jakubkasprzak6899@gmail.com>
2021-01-16 17:11:27 +0100

    fix adjustments

--- include/DateConverter.h
+++ include/DateConverter.h
@@ -8,7 +8,7 @@ public:
     static std::string glue(std::string d, std::string m, std::string r)
     {
         std::stringstream stream;
-        stream << std::setfill ('0') 
+        stream << std::setfill ('0') << std::right
         << std::setw(2) << d
         << std::setw(1) << "-" 
         << std::setw(2) << m 
